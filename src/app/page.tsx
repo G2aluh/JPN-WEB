2ff07589f597a1d97fd4ba9251b9475c6fe981d9
Shinra <galuhputrana576@gmail.com>
2026-05-31 22:54:43 +0700

    JPN13

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1021,7 +1021,7 @@ export default function HomePage() {
           <div className="pt-4 flex justify-center">
             <button
               onClick={() => setShowQrisModal(true)}
-              className="w-full py-3 px-6 rounded-xl font-bold flex items-center bg-red-500 bg-opacity-40 border border-red-500 justify-center gap-2 text-sm  text-white hover:brightness-110 active:brightness-95 transition "
+              className="w-full px-6 rounded-xl font-bold flex items-center bg-red-500/50 border border-red-500 justify-center gap-2 text-sm  text-white hover:brightness-110 active:brightness-95 transition "
             >
               <span>Buy me a Coffee ☕</span>
             </button>
@@ -1054,7 +1054,7 @@ export default function HomePage() {
           >
             <button
               onClick={() => setShowQrisModal(true)}
-              className="w-full py-2.5 px-6 rounded-xl font-bold flex items-center justify-center gap-2 text-sm bg-red-500 bg-opacity-40 border border-red-500 shadow-lg shadow-red-500/50 text-white hover:brightness-110 active:brightness-95 transition "
+              className="w-full py-2.5 px-6 rounded-xl font-bold flex items-center justify-center gap-2 text-sm bg-red-500/50 border border-red-500 shadow-lg shadow-red-500/50 text-white hover:brightness-110 active:brightness-95 transition "
             >
               <span>Buy me a Coffee ☕</span>
             </button>
